72d924ea03eda355a950e33af1d23c594aca333a
a-ok123 <ak@keyideagroup.com>
2021-06-22 16:10:04 -0400

    added 17 nodes datadris and python script to start 17 nodes

---
+++ regtest/start.py
@@ -0,0 +1,17 @@
+./pasteld -datadir=node0  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=91sY9h4AQ62bAhNk1aJ7uJeSnQzSFtz7QmW5imrKmiACm7QJLXe
+./pasteld -datadir=node1  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=923JtwGJqK6mwmzVkLiG6mbLkhk1ofKE1addiM8CYpCHFdHDNGo
+./pasteld -datadir=node2  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=91wLgtFJxdSRLJGTtbzns5YQYFtyYLwHhqgj19qnrLCa1j5Hp5Z
+./pasteld -datadir=node3  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92XctTrjQbRwEAAMNEwKqbiSAJsBNuiR2B8vhkzDX4ZWQXrckZv
+./pasteld -datadir=node4  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=923JCnYet1pNehN6Dy4Ddta1cXnmpSiZSLbtB9sMRM1r85TWym6
+./pasteld -datadir=node5  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=93BdbmxmGp6EtxFEX17FNqs2rQfLD5FMPWoN1W58KEQR24p8A6j
+./pasteld -datadir=node6  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92av9uhRBgwv5ugeNDrioyDJ6TADrM6SP7xoEqGMnRPn25nzviq
+./pasteld -datadir=node7  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=91oHXFR2NVpUtBiJk37i8oBMChaQRbGjhnzWjN9KQ8LeAW7JBdN
+./pasteld -datadir=node8  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92MwGh67mKTcPPTCMpBA6tPkEE5AK3ydd87VPn8rNxtzCmYf9Yb
+./pasteld -datadir=node9  -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92VSXXnFgArfsiQwuzxSAjSRuDkuirE1Vf7KvSX7JE51dExXtrc
+./pasteld -datadir=node10 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=91hruvJfyRFjo7JMKnAPqCXAMiJqecSfzn9vKWBck2bKJ9CCRuo
+./pasteld -datadir=node11 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92sYv5JQHzn3UDU6sYe5kWdoSWEc6B98nyY5JN7FnTTreP8UNrq
+./pasteld -datadir=node12 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance -masternode -txindex=1 -reindex -masternodeprivkey=92pfBHQaf5K2XBnFjhLaALjhCqV8Age3qUgJ8j8oDB5eESFErsM
+./pasteld -datadir=node13 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance
+./pasteld -datadir=node14 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance
+./pasteld -datadir=node15 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance
+./pasteld -datadir=node16 -daemon -keypool=1 -discover=0 -rest -debug=masternode,mnpayments,governance
